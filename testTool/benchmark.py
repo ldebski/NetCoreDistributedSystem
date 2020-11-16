c354092d47c1cdb2e8ab04b12fdef831fd892f70
Raciniewska <40673647+Raciniewska@users.noreply.github.com>
2020-11-16 12:02:44 +0100

    Add files via upload

---
+++ testTool/benchmark.py
@@ -0,0 +1,87 @@
+import random
+import asyncio
+import sys
+
+from aiohttp import ClientSession
+import pyodbc
+import pandas as pd
+
+async def fetch(url, session):
+    async with session.get(url) as response:
+        #print(response.status)
+        if response.status != 200:
+            raise NameError("RESPONSE STATUS: "+response.status)
+        return await response.read()
+
+async def bound_fetch(sem, url, session):
+    # Getter function with semaphore.
+    async with sem:
+        await fetch(url, session)
+
+def getCashQuery():
+    cnxn_str = ("Driver={SQL Server};"
+                "Server=localhost;"
+                "Database=BankDataBase;"
+                "UID=sa;"
+                "PWD=STRONGpassword123!;")
+    cnxn = pyodbc.connect(cnxn_str)
+    query = "SELECT * FROM Account"
+    df_before = pd.read_sql_query(query, cnxn)
+    return df_before['CashAmount'].tolist()
+
+async def run(r, t):
+    tasks = []
+    # create instance of Semaphore
+    sem = asyncio.Semaphore(t)
+
+    # Create client session that will ensure we dont open new connection
+    # per each request.
+    async with ClientSession() as session:
+        for i in range(r):
+            from_id.append(random.randint(0, len(cashAmountBefore) - 1))
+            to_id.append(random.randint(0, len(cashAmountBefore) - 1))
+            amount.append(random.randint(1, 1000))
+            url = f'http://localhost:8000/send/przelew/{from_id[-1]}/{to_id[-1]}/{amount[-1]}'
+            # pass Semaphore and session to every GET request
+            task = asyncio.ensure_future(bound_fetch(sem, url.format(i), session))
+            tasks.append(task)
+
+        responses = asyncio.gather(*tasks)
+        await responses
+
+
+number_of_requests = int(sys.argv[1])
+number_of_concurrent_tasks = 100
+
+print("************Starting program************\n")
+print("Creating "+str(number_of_requests)+"requests, where number of concurrent tasks is " + str(number_of_concurrent_tasks))
+
+from_id = []
+to_id = []
+amount = []
+cashAmountBefore = getCashQuery()
+
+loop = asyncio.get_event_loop()
+
+future = asyncio.ensure_future(run(number_of_requests, number_of_concurrent_tasks))
+loop.run_until_complete(future)
+
+
+for index in range(len(from_id)):
+    if cashAmountBefore[from_id[index]] >= amount[index]:
+        cashAmountBefore[from_id[index]] -= amount[index]
+        cashAmountBefore[to_id[index]] += amount[index]
+
+print("Press enter when tasks end")
+input()
+
+cashAmountAfter = getCashQuery()
+
+for index in range(len(cashAmountBefore)):
+    if cashAmountBefore[index] != cashAmountAfter[index]:
+        print("Error at id="+index)
+        print("Amount in database is:" + cashAmountAfter[index])
+        print("Should be:" + cashAmountBefore[index])
+
+print("TESTS PASSED")
+print("***********Ending program************\n")
